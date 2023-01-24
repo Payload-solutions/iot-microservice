19bfc6fed8a281032003893e94bdbf60189fd84d
Arturo0911 <anegreiross@outlook.com>
2023-01-24 09:57:44 -0500

    udpating values

--- models/real_time_soil_values.go
+++ models/real_time_soil_values.go
@@ -1,15 +1,19 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type RealEnvironmentValues struct {
-	SoilId             int64   `json:"soil_id"`
-	TemperatureSoil    float64 `json:"temperature_soil"`
-	TemperatureEnviron float64 `json:"temperature_environ"`
-	PH                 float64 `json:"ph"`
-	Humidity           float64 `json:"humidity"`
-	Dates              string  `json:"dates"`
-	SoilState          int     `json:"soil_state"`
+	SoilId             int64     `gorm:"primary_key;auto_increment;not_null" json:"soil_id"`
+	SoilDates          time.Time `gorm:"type:timestamp;default:current_timestamp" json:"soil_dates"`
+	TemperatureSoil    float64   `json:"temperature_soil"`
+	TemperatureEnviron float64   `json:"temperature_environ"`
+	PH                 float64   `json:"ph"`
+	Humidity           float64   `json:"humidity"`
+	SoilState          int       `gorm:"type:int;default:1" json:"soil_state"`
 }
 
 func CreateEnviron(db *gorm.DB, reader *RealEnvironmentValues) (err error) {
